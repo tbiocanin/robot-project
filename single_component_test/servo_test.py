c9e547ae381f616fe8a61a7961949eddf4de40a8
Teodor Biocanin <tekikralj011@gmail.com>
2021-11-15 23:27:11 +0100

    test script working
    one servo needs to be changed

--- single_component_test/servo_test.py
+++ single_component_test/servo_test.py
@@ -1,22 +1,15 @@
 # uvoz funkcije za adeept
 import Adafruit_PCA9685
+from adafruit_servokit import ServoKit
 import time
 
-init_position = 300
-max_position = 600
-min_position = 100
+kit = ServoKit(channels=16)
 
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(50)
-# servo.set_pwm(0, 0, 300)
 while True:
-    pwm.set_pwm(0, 0, init_position)
+    kit.servo[1].angle = 180
     time.sleep(1)
-    pwm.set_pwm(0, 0, max_position)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, min_position)
-    time.sleep(1)
-# servo_motor = Adafruit_PCA9685.Servos()
+    kit.servo[1].angle = 0
+
 
 # if __name__ == "__main__":
 #     pass
